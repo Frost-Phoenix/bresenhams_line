b1d8d9132ef6dea61ff69794bcb63b69d91f98d8
Frost-Phoenix <67cyril6767@gmail.com>
2025-03-28 16:39:24 +0100

    6502_asm: add documentation

--- 6502/line.asm
+++ 6502/line.asm
@@ -1,9 +1,20 @@
 ; Bresenham's line drawing algorithm
 ; https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm#All_cases
 
+; Usage:
+;
+;     Move the cursor with <W> <A> <S> <D> and press <ENTER> to select the
+;   pixel at the cursor location. The first selected pixel will be the start
+;   of the line, and the second one its end. After selecting the end pixel
+;   the line will be drawn. To reset the screen and the cursor position
+;   press <R>.
+
 ; #--------------------------------------------# ;
 
-define  pixel_current_L $00
+; see memory layout in ./memory_layout.ods
+; uses only zeropage
+
+define  pixel_current_L $00 ; stores screen addrs at witch pixel will be drawn
 define  pixel_current_H $01
 define  pixel_current_x $02
 define  pixel_current_y $03
@@ -16,8 +27,13 @@ define  pixel_end_y     $09
 define  pixel_swap_x    $0a
 define  pixel_swap_y    $0b
 
+; next pixel to be selected
+;   0 -> start pixel
+;   1 -> end pixel
+;   2 -> both pixels selected, draw line
 define  pixel_current_selection $0d
 
+; constants used in the line algorithm
 define  _dx     $10
 define  _dy     $11
 define  _2dx    $12
@@ -27,14 +43,16 @@ define  _dir_y  $15
 define  _abs_dx $16
 define  _abs_dy $17
 
+; variables / constants used in the line algorithm
 define  _D        $20
 define  _D_step_H $21 ; 2 * (dy - dx)
 define  _D_step_V $22 ; 2 * (dx - dy)
 
-define  color_current_pixel $40
+define  color_current_pixel $40 ; color under the cursor
 define  color_drawing       $41
 define  color_cursor        $42
 
+; ascii codes of keys
 define  keycode_w     $77
 define  keycode_a     $61
 define  keycode_s     $73
@@ -42,17 +60,21 @@ define  keycode_d     $64
 define  keycode_r     $72
 define  keycode_enter $0d
 
-; System variables
+; system variables
 define  keycode_pressed $ff
 
 ; #--------------------------------------------# ;
 
-lda #$0f
-sta pixel_cursor_x
-lda #$0f
-sta pixel_cursor_y
-lda #$0c
-sta color_cursor
+init:
+  ; starting cursor position
+  lda #$0f
+  sta pixel_cursor_x
+  sta pixel_cursor_y
+  ; cursor color
+  lda #$0c
+  sta color_cursor
+  ; X register should always be 0
+  ldx #$00
 
 ; #--------------------------------------------# ;
 
@@ -67,14 +89,14 @@ main_loop:
 
 blink_cursor_pixel:
   jsr copy_cursor_to_current
-  ; save current pixel color
+  ; save the color under the cursor, so it is not overwriten
   lda (pixel_current_L, X)
   sta color_current_pixel
   ; set current pixel color to cursor color
   lda color_cursor
   sta (pixel_current_L, X)
   jsr delay
-  ; restore its original color
+  ; restore original pixel color
   lda color_current_pixel
   sta (pixel_current_L, X)
   jsr delay
@@ -103,7 +125,7 @@ update_keyboard:
 
 move_up:
   lda pixel_cursor_y
-  ; if 0: can't move up
+  ; if y == 0: can't move up
   beq out_of_bound
   dec pixel_cursor_y
 
@@ -120,7 +142,7 @@ move_down:
 
 move_left:
   lda pixel_cursor_x
-  ; if 0: can't move left
+  ; if x == 0: can't move left
   beq out_of_bound
   dec pixel_cursor_x
 
@@ -136,9 +158,11 @@ move_right:
   jmp update_keyboard_end
 
 reset:
+  ; replace cursor at the middle of the screen
   lda #$0f
   sta pixel_cursor_x
   sta pixel_cursor_y
+  ; no pixel selected, next start
   lda #$00
   sta pixel_current_selection
 
@@ -146,28 +170,35 @@ reset:
 
   jmp update_keyboard_end
 
+; select starting / end pixel coordinates
+; if both selected: draw line
 select_pixel:
+  ; test if start has been selected or not
   lda pixel_current_selection
   beq select_pixel_start
   jmp select_pixel_end
 
   select_pixel_start:
+    ; copy cursor pos to start pos
     lda pixel_cursor_x
     sta pixel_start_x
     lda pixel_cursor_y
     sta pixel_start_y
     jmp select_end
   select_pixel_end:
+    ; copy cursor pos to end pos
     lda pixel_cursor_x
     sta pixel_end_x
     lda pixel_cursor_y
     sta pixel_end_y
     jmp select_end
   select_end:
+    ; draw red pixel under the cursor to indicate the selected pixel
     lda #$02
     sta color_drawing
     jsr copy_cursor_to_current
     jsr plot_pixel
+    ; a pixel has been selected, go to next step
     inc pixel_current_selection
     ; if both start end end selected, draw line
     lda pixel_current_selection
@@ -191,6 +222,7 @@ update_keyboard_end:
 
 ; #--------------------------------------------# ;
 
+; initialize constants used by line algorithm
 init_line:
   ; dx
   lda pixel_end_x
@@ -232,6 +264,7 @@ init_line_horizontal:
   lda #$01
   sta _dir_y
   lda _dy
+  ; if dy >= 0: continue
   bpl init_line_horizontal_continue
   lda #$ff
   sta _dir_y
@@ -270,6 +303,7 @@ init_line_vertical:
   lda #$01
   sta _dir_x
   lda _dx
+  ; if dx >= 0: continue
   bpl init_line_vertical_continue
   lda #$ff
   sta _dir_x
@@ -303,27 +337,31 @@ init_line_vertical_continue:
 
 ; #--------------------------------------------# ;
 
+; general draw line:
+;   - choose between vertical or horizontal line
+;   - if necessary invert start and end pos
 draw_line:
   jsr init_line
 
   ; if abs(dy) < abs(dx)
   lda _abs_dy
   cmp _abs_dx
-  ; if true:
+  ; if true: use horizontal line
   bcc _horizontal
-  ; if false:
+  ; if false: use vertical line
   jmp _vertical
 
   _horizontal:
     ; if start_x > end_x:
     lda pixel_start_x
     cmp pixel_end_x
+    ; if true: swap start and end pos
     bcs _horizontal_swap
     jsr draw_line_horizontal
     jmp _end
   _horizontal_swap:
     jsr swap_start_end_pos
-    ; dx and dy changes since pos swap, so neet to be recalculated
+    ; dx and dy changes since pos swap, so need to be recalculated
     jsr init_line
     jsr draw_line_horizontal
     jmp _end
@@ -331,12 +369,13 @@ draw_line:
     ; if start_y > end_y:
     lda pixel_start_y
     cmp pixel_end_y
+    ; if true: swap start and end pos
     bcs _vertical_swap
     jsr draw_line_vertical
     jmp _end
   _vertical_swap:
     jsr swap_start_end_pos
-    ; dx and dy changes since pos swap, so neet to be recalculated
+    ; dx and dy changes since pos swap, so need to be recalculated
     jsr init_line
     jsr draw_line_vertical
     jmp _end
@@ -346,6 +385,7 @@ draw_line:
 
 ; #--------------------------------------------# ;
 
+; horizontal line algorithm, see ../resources/line_horizontal.png
 draw_line_horizontal:
   jsr init_line_horizontal
 
@@ -386,6 +426,7 @@ draw_line_horizontal:
 
 ; #--------------------------------------------# ;
 
+; horizontal line algorithm, see ../resources/line_vertical.png
 draw_line_vertical:
   jsr init_line_vertical
 
@@ -426,7 +467,7 @@ draw_line_vertical:
 
 ; #--------------------------------------------# ;
 
-; convert current pixel x,y to addr
+; convert current pixel x,y to screen addr
 ; pixel_current_addr = $200 + y * 32 + x
 pos_to_addr:
   ; screens starts at $200
@@ -437,6 +478,7 @@ pos_to_addr:
 
   ldy pixel_current_y
 
+  ; add 32 y times
   pta_loop_y:
     cpy #$00
     beq pta_x
@@ -456,6 +498,7 @@ pos_to_addr:
 
     jmp pta_loop_y
 
+  ; add x
   pta_x:
     lda pixel_current_L
     clc
@@ -466,22 +509,11 @@ pos_to_addr:
 
 ; #--------------------------------------------# ;
 
-; plot white pixel at the position saved in memory in pixelAddrL
+; plot a pixel of color color_drawing at the
+; position saved in memory in pixelAddrL
 plot_pixel:
-  ; save X and A registers
-  pha
-  txa
-  pha
-  ldx #$00
-
-  ; plot pixel
   lda color_drawing
   sta (pixel_current_L, X)
-
-  ; restore X and A registers
-  pla
-  tax
-  pla
 
   rts
 
@@ -508,6 +540,7 @@ swap_start_end_pos:
 
 ; #--------------------------------------------# ;
 
+; copy cursor x,y to current pixel x,y
 copy_cursor_to_current:
   lda pixel_cursor_x
   sta pixel_current_x
@@ -518,6 +551,8 @@ copy_cursor_to_current:
 
 ; #--------------------------------------------# ;
 
+; reset screen to all black
+; go first to all white for small animation
 reset_screen:
   lda #$01
   reset_screen_loop_white:
@@ -541,6 +576,7 @@ reset_screen:
 
 ; #--------------------------------------------# ;
 
+; negate number in accumulator
 negate:
   eor #$ff
   clc
